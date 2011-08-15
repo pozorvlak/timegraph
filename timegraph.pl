1280f342003d9d91370210b3fbfe7073a82fb529
Miles Gould <miles@assyrian.org.uk>
2011-08-16 00:50:48 +0100

    Added header line

--- timegraph.pl
+++ timegraph.pl
@@ -5,10 +5,11 @@ use Time::HiRes qw/gettimeofday tv_interval/;
 use autodie;
 
 my $command = shift @ARGV;
+say "Argument\tTime";
 for my $arg (@ARGV) {
     my $start = [gettimeofday()];
     open my $pipe, "-|", "$command $arg";
     my @lines = <$pipe>;
     my $duration = tv_interval($start);
-    say STDOUT "$arg\t$duration";
+    say "$arg\t$duration";
 }
